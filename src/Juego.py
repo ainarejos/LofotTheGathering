baa59745bfeb2d0ad8c59b03b8aa8e15905f3805
ainarejos <ainarejos2@nigul.cide.es>
2021-04-29 20:07:09 +0200

    Added enemy functions and solved some bugs.

--- src/Juego.py
+++ src/Juego.py
@@ -1,5 +1,6 @@
 from src.Personaje import Personaje
 from src.Tecnica import Tecnica
+from random import randint
 class Juego:
 
     def __init__(self, nombre):
@@ -12,65 +13,168 @@ class Juego:
         self.jugador1=Personaje;
         self.jugador2=Personaje;
 
+
     def carga(self):
-        self.t1 = Tecnica("Gancho", 15, 0, 60, 85, 20)
-        self.t2 = Tecnica("Rayo Solar", 0, 30, 20, 70, 35)
-        self.p1 = Personaje("Moradin", 80, 30, 10, 100)
-        self.p2= Personaje("Gandalf", 80, 30, 10, 100)
+        self.t1 = Tecnica("Gancho", 50, 0, 60, 85, 20)
+        self.t2 = Tecnica("Rayo Solar", 0, 50, 20, 70, 35)
+        self.p1 = Personaje("Moradin", 200, 30, 10, 100)
+        self.p2= Personaje("Gandalf", 200, 30, 10, 100)
+        self.p3 = Personaje("Tofol", 100000, 100000, 1000000, 1000000000)
+
         self.personajes.append(self.p1)
         self.personajes.append(self.p2)
+        self.personajes.append(self.p3)
         self.p1.setTecinas(self.t1, self.t2)
         self.p2.setTecinas(self.t1, self.t2)
+        self.p3.setTecinas(self.t1, self.t2)
         #self.p1.getTecnica()
 
-    def seleccion(self):
-        print("Lofot The Gathering\n-------------------" )
+    def juego(self):
+        print("-------------------------------------------------\n _            __      _     _______ _\n"+
+              "| |          / _|    | |   |__   __| |\n"+
+"| |     ___ | |_ ___ | |_     | |  | |__   ___\n"+
+"| |    / _ \|  _/ _ \| __|    | |  | '_ \ / _ \ \n"+
+"| |___| (_) | || (_) | |_     | |  | | | |  __/ \n"+
+"|______\___/|_| \___/ \__|    |_|  |_| |_|\___|\n" +
+  " _____        _   _               _ \n"+
+ "/  ____|     | | | |             (_)\n"+
+"| |  __  __ _| |_| |__   ___ _ __ _ _ __   __ _ \n"+
+"| | |_ |/ _` | __| '_ \ / _ \ '__| | '_ \ / _` |\n"+
+"| |__| | (_| | |_| | | |  __/ |  | | | | | (_| |\n"+
+"\_____|\__,_|\__|_| |_|\___| _|  |_|_| |_|\__, |\n"+
+                                           "\t\t\t\t\t\t\t\t\t\t   __/ |\n"+
+                                          "\t\t\t\t\t\t\t\t\t\t  |___/"+"\n-------------------------------------------------" )
+        self.listadoPersonajes()
+        self.seleccion()
+
+    def listadoPersonajes(self):
         count=0
         print("Personajes:")
-        print("------------")
+        print("-------------------------------------------------")
         for x in self.personajes:
             count = count + 1
             print(str(count) + " - " + str(x.getNombre()))
-        print("------------")
-        j1=input("Jugador 1, seleccione un personaje: ")
-        if j1=="1":
-            self.jugador1=self.p1
-        elif j1=="2":
-            self.jugador1=self.p2
-        print("El jugador 1 ha seleccionado a: " + str(self.jugador1.getNombre()))
         print("-------------------------------------------------")
-        j2 = input("Jugador 2, seleccione un personaje: ")
-        if j2 == "1":
-            self.jugador2 = self.p1
-        elif j2 == "2":
-            self.jugador2 = self.p2
-        print("El jugador 2 ha seleccionado a: " + str(self.jugador2.getNombre()))
+
+    def seleccion(self):
+
+        j=input("-------------------------------------------------\n"+ "Adalid, por favor, seleccione un personaje: ")
+        if j=="1":
+            self.p1.descPersonaje()
+            eleccion=input("Estas seguro que deseas utilizar este campeon?\n1-- Si\n2-- No\nRespuesta: ")
+            if eleccion=="1":
+                self.jugador1=self.p1
+                print( "El jugador ha seleccionado a: " + str(self.jugador1.getNombre()))
+                print("-------------------------------------------------")
+
+            elif eleccion=="2":
+                print("-------------------------------------------------\nVolviendo a cargar los personajes...\n-------------------------------------------------")
+                self.listadoPersonajes()
+                self.seleccion()
+        elif j=="2":
+            self.p2.descPersonaje()
+            eleccion = input("Estas seguro que deseas utilizar este campeon?\n1-- Si\n2-- No\nRespuesta: ")
+            if eleccion == "1":
+                self.jugador1 = self.p2
+                print("El jugador ha seleccionado a: " + str(self.jugador1.getNombre()))
+                print("-------------------------------------------------")
+            elif eleccion == "2":
+                print(
+                    "-------------------------------------------------\nVolviendo a cargar los personajes...\n-------------------------------------------------")
+                self.listadoPersonajes()
+                self.seleccion()
+
+        elif j=="3":
+            self.p3.descPersonaje()
+            eleccion = input("Estas seguro que deseas utilizar este campeon?\n1-- Si\n2-- No\nRespuesta: ")
+            if eleccion == "1":
+                self.jugador1 = self.p3
+                print("El jugador ha seleccionado a: " + str(self.jugador1.getNombre()))
+                print("-------------------------------------------------")
+            elif eleccion == "2":
+                print(
+                    "-------------------------------------------------\nVolviendo a cargar los personajes...\n-------------------------------------------------")
+                self.listadoPersonajes()
+                self.seleccion()
+        else :
+            print ("Opcion incorrecta!")
+            self.seleccion()
 
     def usarTecnica(self, jugador, enemigo):
-        print("----------------------")
+        print("-------------------------------------------------")
         print("Tecnicas de: " + jugador.getNombre())
-        print(jugador.getTecnica())
+        jugador.getTecnica()
+
         tec=input("Seleccione una tecnica: ")
         if tec=="1":
-            enemigo.recibirDanyo(int(jugador.getTecnica1(0).getDanyoFisico()))
-            print(enemigo.getVida())
+            if (jugador.getTecnica1(0).getDanyoFisico()) > 0:
+                enemigo.recibirDanyoFisico(int(jugador.getTecnica1(0).getDanyoFisico()))
+            elif (jugador.getTecnica1(0).getDanyoMagico()) > 0:
+                enemigo.recibirDanyoMagico(int(jugador.getTecnica1(0).getDanyoMagico()))
+            print("Tecnica realizada correctamente, la vida de " + enemigo.getNombre() +  " es: " + str(enemigo.getVida()))
+
+        elif tec=="2":
+            if (jugador.getTecnica1(1).getDanyoFisico()) > 0:
+                enemigo.recibirDanyoFisico(int(jugador.getTecnica1(1).getDanyoFisico()))
+            elif (jugador.getTecnica1(1).getDanyoMagico()) > 0:
+                enemigo.recibirDanyoMagico(int(jugador.getTecnica1(1).getDanyoMagico()))
+            print("Tecnica realizada correctamente, la vida de " + enemigo.getNombre() +  " es: " + str(enemigo.getVida()))
+        else:
+            print("Opcion incorrecta, elija una tecnica valida")
+            self.usarTecnica(jugador, enemigo)
+
+    def eleccionBoss(self):
+        r=randint(1,3)
+        if r==1:
+            self.jugador2=self.p1
+        elif r==2:
+            self.jugador2=self.p2
+        elif r==3:
+            self.jugador2=self.p3
 
 
+    def bossAttacks(self):
+        r=randint(1,2)
+        print("-------------------------------------------------\nTurno de: "+self.jugador2.getNombre()+ " enemigo\n-------------------------------------------------")
+        if r==1:
+            if (self.jugador2.getTecnica1(0).getDanyoFisico()) > 0:
+                self.jugador1.recibirDanyoFisico(int(self.jugador2.getTecnica1(0).getDanyoFisico()))
+                print(self.jugador2.getNombre() +" va a usar "+ self.jugador2.getTecnica1(0).getNombre() )
+            elif (self.jugador1.getTecnica1(0).getDanyoMagico()) > 0:
+                self.jugador1.recibirDanyoMagico(int(self.jugador2.getTecnica1(0).getDanyoMagico()))
+                print(self.jugador2.getNombre() + " va a usar " + self.jugador2.getTecnica1(0).getNombre())
 
+            print("Tecnica realizada correctamente, la vida de " + self.jugador1.getNombre() +  " es: " + str(self.jugador1.getVida()))
+        elif r==2:
+            if (self.jugador2.getTecnica1(1).getDanyoFisico()) > 0:
+                self.jugador1.recibirDanyoFisico(int(self.jugador2.getTecnica1(1).getDanyoFisico()))
+                print(self.jugador2.getNombre() + " va a usar " + self.jugador2.getTecnica1(1).getNombre())
+            elif (self.jugador1.getTecnica1(1).getDanyoMagico()) > 0:
+                self.jugador1.recibirDanyoMagico(int(self.jugador2.getTecnica1(1).getDanyoMagico()))
+                print(self.jugador2.getNombre() + " va a usar " + self.jugador2.getTecnica1(1).getNombre())
+
+            print("Tecnica realizada correctamente, la vida de " + self.jugador1.getNombre() + " es: " + str(self.jugador1.getVida()))
 
     def lucha(self):
-        print("Que empiecen los Juegos del Hambre")
+        print("Que empiecen la arena")
+        self.eleccionBoss()
         rondas=0;
+        print("\n-------------------------------------------------\n"+self.jugador1.getNombre() + " VS " + self.jugador2.getNombre())
         while self.jugador1.getVida()>0 and self.jugador2.getVida()>0:
             rondas=rondas+1;
-            print("Ronda: " + str(rondas))
+            print("-------------------------------------------------\nRonda: "+ str(rondas))
             self.usarTecnica(self.jugador1, self.jugador2)
-            print("patata")
-            break
 
-
+            if (self.jugador2.getVida()>0):
+                self.bossAttacks()
+        if (self.jugador1.getVida()<=0):
+            print("\n-------------------------")
+            print("Ha ganado:" + str(self.jugador2.getNombre()))
+        else:
+            print("\n-------------------------")
+            print("Ha ganado:" + str(self.jugador1.getNombre()))
 
 j1 = Juego("LofotTheGathering")
 j1.carga()
-j1.seleccion()
+j1.juego()
 j1.lucha()
--- src/Personaje.py
+++ src/Personaje.py
@@ -7,23 +7,33 @@ class Personaje:
         self.Armadura=armadura
         self.Resist_Magica=resist_Magica
         self.Energia=energia;
-        self.Tecnicas=[]
+        self.Tecnicas = []
 
     def getNombre(self):
         return self.Nombre
+
     def getVida(self):
         return self.vida
+
     def getTecnica(self):
-        cont=0
+        cont = 0
         for x in self.Tecnicas:
-            cont=cont+1
+            cont = cont + 1
             print(str(cont) + " - " + str(x.getNombre()))
+
     def getTecnica1(self, int):
         return self.Tecnicas[int]
+        print(str(x.getNombre()))
+
     def getArmadura(self):
         return self.Armadura
+
     def getResist_Magica(self):
-        return self.Resist_Magica
+        return  self.Resist_Magica
+
+
+
+
     def getEnergia(self):
         return self.Energia
 
@@ -31,5 +41,28 @@ class Personaje:
         self.Tecnicas.append(Tecnica1)
         self.Tecnicas.append(Tecnica2)
 
-    def recibirDanyo(self, int):
-        self.vida=self.vida-int
+    def recibirDanyoFisico(self, int):
+        if (int > self.Armadura):
+            self.vida = self.vida - int + self.Armadura
+            print("")
+        else:
+            print("La armadura paro el ataque")
+
+    def recibirDanyoMagico(self, int):
+        if (int > self.Resist_Magica):
+            self.vida = self.vida - int + self.Resist_Magica
+        else:
+            print(str(self.Nombre) + " resistió la tecnica")
+
+    def descPersonaje(self, ):
+        print("-------------------------------------------------\n"
+              "Has elegido un campeón para luchar en la gran "
+              "\narena de Lofot, analiza a tu campeón, adalid, "
+              "\ny decide si deseas continuar o elegir a otro."
+              "\n-------------------------------------------------"
+              "\n|| Nombre               ===> " + self.Nombre +
+              "\n|| Vida                 ===> " + str(self.vida) +
+              "\n|| Armadura             ===> " + str(self.Armadura) +
+              "\n|| Resistencia Mágica   ===> " + str(self.Resist_Magica) +
+              "\n|| Energia              ===> " + str(self.Energia)
+              )
